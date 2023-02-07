ea2d17e8b4f4b7dc832d3500d841aac146744b3c
tammyghamad <122330778+tammyghamad@users.noreply.github.com>
2023-02-07 23:52:01 +0300

    Add files via upload

---
+++ tamarahamad811 2/js/index.js
@@ -0,0 +1,20 @@
+console.log("Your js include succesfully");
+
+function clickEvent() {
+    console.log("You have click on page title");
+}
+function  imageMouseOver() {
+    console.log("YOu have over on image")
+
+}
+function scrollcheck() {
+    console.log("You have scroll a div");
+}
+
+function checkOnLoad(){
+    console.log("checkin page onload");
+};
+
+window.onscroll = function() {
+    console.log("You are scrolling the page.")
+}
